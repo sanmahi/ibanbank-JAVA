ed8c96117ab737f8f515f64468cf4c6e0a82b9d6
User1 <User1@LP-5CD9176L7J.HCLT.CORP.HCL.IN>
2019-09-05 16:54:52 +0530

    issue fic

--- src/main/java/com/iban/IbanBankApplication.java
+++ src/main/java/com/iban/IbanBankApplication.java
@@ -7,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IbanBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IbanBankApplication.class, args);
-		
-		
-				String s1="adA12", s2="jh@l-'";
-				System.out.println(s1.matches("[a-zA-Z0-9'-]+"));
-				System.out.println(s2.matches("[a-zA-Z0-9]+"));
-			
+		SpringApplication.run(IbanBankApplication.class, args);		
 		}
 	}
